c990d99737a8cfcb9a0fe1bad7aa67c3713bc1c0
Zypeh <Zypeh.geek@gmail.com>
2016-01-30 16:19:21 +0800

    arguments parsing

--- touka.go
+++ touka.go
@@ -3,15 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/docopt/docopt-go"
-	"github.com/zypeh/touka/gist"
 )
 
 func main () {
 	usage := `Touka
 
 Usage:
-  touka push [private] <filename>... 
-  touka list
+  touka push [private] <filename>...
+  touka list 
   touka -h | --help
   touka --version
 
@@ -19,7 +18,22 @@ Option:
   -h --help    Show this screen.
   --version    Show version
 `
-	arguments, _ := docopt.Parse (usage, nil, true, "touka", false)
+	// func Parse (doc string, argv []string, help bool, version string,
+	// optionsFirst bool, exit ...bool) (map[string]interface{}, error)
+	arguments, _ := docopt.Parse (usage, nil, true, "0.1.0", false)
 	fmt.Println (arguments)
-	
+
+	if arguments ["push"] == true {
+		if arguments ["private"] == true {
+			fmt.Println ("touka private push")
+		} else {
+			fmt.Println ("touka public push")
+		}
+	}	
+
+	if arguments ["list"] == true {
+		// TODO: interactive
+		fmt.Println ("touka list !!!")
+	}
+   
 }
