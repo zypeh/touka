af66b0b6cc99f316cfb7f24b9b3d9a1173a7dc10
Zypeh <Zypeh.geek@gmail.com>
2016-01-30 17:20:37 +0800

    gist user list

--- gist.go
+++ gist.go
@@ -1,6 +1,5 @@
 // This code is part of the touka
-
-package gist
+package Gist
 
 import (
 	"bytes"
@@ -10,12 +9,14 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/fatih/color"
 )
 
 // GIT_IO_URL is the github's URL shortner
 // API v3 is the current version of Github API
 const (
-	GITHUB_API_URL = "https://api.github.com/gists"
+	GITHUB_API_URL = "https://api.github.com/"
 	GIT_IO_URL = "http://git.io"
 	GIT_BASE_PATH = "/api/v3"
 )
@@ -32,6 +33,15 @@ var (
 	responseObj map [string]interface{}
 )
 
+/* Color initialization */
+var (
+	yellow := color.New (color.FgYellow).SprintfFunc ()
+	yellow_printf := color.New (color.FgYellow).PrintfFunc ()
+
+	green := color.New (color.FgGreen).SprintfFunc ()
+	green_printf := color.New (color.FgGreen).PrintfFunc ()
+)
+
 type GistFile struct {
 	Content string `json:\"content\"`
 }
@@ -42,6 +52,7 @@ type Gist struct {
 	GistFile    map [string] GistFile   `json:\"files\"`
 }
 
+/* Load Token From ~/.gist */
 func loadTokenFromFIle () (token string) {
 	file := os.Getenv ("$HOME") + "/.gist"
 	github_token, err := ioutil.ReadFile (file)
@@ -51,6 +62,15 @@ func loadTokenFromFIle () (token string) {
 	return string (github_token)
 }
 
+/* Error handling */
+func errhandler (handler err) {
+	if handler != nil {
+		log.Fatal (handler)
+		os.Exit (1)
+	}
+}
+
+/* Push the file on gist.github.com */
 func Push (file Gist) {
 	pfile, err := json.Marshal (file)
 
@@ -68,5 +88,29 @@ func Push (file Gist) {
 		req.Headet.Set ("Accept", "application/json")
 		req.SetBasicAuth (token, "x-oauth-basic")
 	}
+}	
 
+func List (url, token string) {
+
+	if token != "" {
+		url = url + "?access_token=" + token
+	}
+
+	res, err := http.Get (url)
+	errhandler (err)
+	
+	defer res.Body.Close ()
+	body, err := ioutil.ReadAll (res.Body)
+	errhandler (err)
+
+	var jsonRes []GistJSON.Response
+	err= json.Unmarshal (body, &jsonRes)
+	errhandler (err)
+
+	for _, val := range jsonRes {
+	    yellow_printf ("%sn", val.HtmlUrl)
+		fmt.Printf ("(%s)\t%s\n", shortDate (val.CreatedAt), val.Description)
+		fmt.Printf ("\n")
+	}
+	
 }	
